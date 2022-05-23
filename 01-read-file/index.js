353a93d8bea57ea500d8ad53ce0c2a04a2cea577
EfimenkoAndrey <andrey.efimenko.sp@gmail.com>
2022-05-23 17:45:14 +0300

    feat: add solution for 01-read-file (23 май 2022 г. 17:45:14)

---
+++ 01-read-file/index.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const path = require('path');
+const pathFile = path.join(__dirname, 'text.txt');
+const readStream = fs.createReadStream(pathFile, {encoding: 'utf-8'});
+
+readStream.on('data', (chunk) => {
+	console.log(chunk.toString());
+})
+
