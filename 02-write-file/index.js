f03062a60be2ff39d2daad6142812f362ffd8359
EfimenkoAndrey <andrey.efimenko.sp@gmail.com>
2022-09-03 22:32:10 +0300

    feat: change code for task 02-write-file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,19 +1,20 @@
-const fs = require('fs');
-const path = require('path');
-const pathFile = path.join(__dirname, 'app.txt');
-const readline = require('readline');
+const fs = require('node:fs');
+const path = require('node:path');
+const readline = require('node:readline');
 const rl = readline.createInterface({
-	input:process.stdin,
-	output:process.stdout
+  input: process.stdin,
+  output: process.stdout
 });
-function writeContent(content) {
-	if (content === 'exit') {
-		process.exit()
-	}
-	fs.appendFile(pathFile, content+'\n', err => {
-		if (err) throw err
-	})
-	rl.question('', writeContent);
-};
-rl.question('Hi, write here message!\n', writeContent);
-process.on('exit', () => console.log('Goodbye!'))
+function writable (text) {
+  if (text === 'exit') {
+    process.exit();
+  }
+  fs.appendFile(path.resolve(__dirname, 'app.txt'), text + '\n', (err) => {
+    if (err) {
+      throw err
+    }
+  })
+  rl.question('', writable);
+}
+rl.question("Hi, let's write something! \n", writable);
+process.on('exit', () => console.log('Goodbuy!'))
