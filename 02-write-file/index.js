1d4be026f926f1fbe9bff7ace2e07c0c86008986
EfimenkoAndrey <andrey.efimenko.sp@gmail.com>
2022-11-09 20:18:36 +0300

    fix: fix bag for create file for run programm

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -5,16 +5,21 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
+fs.open(path.resolve(__dirname, 'text.txt'),'w+',(err) => {
+  if (err) {
+    throw err;
+  }
+});
 function writable (text) {
   if (text === 'exit') {
     process.exit();
   }
-  fs.appendFile(path.resolve(__dirname, 'app.txt'), text + '\n', (err) => {
+  fs.appendFile(path.resolve(__dirname, 'text.txt'), text + '\n', (err) => {
     if (err) {
-      throw err
+      throw err;
     }
-  })
+  });
   rl.question('', writable);
 }
-rl.question("Hi, let's write something! \n", writable);
-process.on('exit', () => console.log('Goodbuy!'))
+rl.question('Hi, let\'s write something! \n', writable);
+process.on('exit', () => console.log('Goodbuy!'));
